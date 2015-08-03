97ac79842c1d5900434f98e1c31bcce886873b66
rickeh808 <rickeh808@gmail.com>
2015-08-03 10:23:32 -1000

    added button and procedure

--- app/src/main/java/com/example/ricky/myapplication/Procedure.java
+++ app/src/main/java/com/example/ricky/myapplication/Procedure.java
@@ -1,25 +1,25 @@
 package com.example.ricky.myapplication;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity implements View.OnClickListener
-{
+public class Procedure extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_procedure);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_procedure, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,12 +35,4 @@ public class MainActivity extends Activity implements View.OnClickListener
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onClick(View v)
-    {
-       Intent intent = new Intent(getApplicationContext(),Procedure.class);
-       startActivity(intent);
-    }
 }
-//aa
